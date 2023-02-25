eec9f37460f27460ade667384ecb61cc4e8317b8
Станислав <113885026+StanislavKon@users.noreply.github.com>
2023-02-25 21:21:57 +0300

    hw4

---
+++ homeworks.4/MyQueue.java
@@ -0,0 +1,36 @@
+package java.homeworks4;
+
+import java.util.LinkedList;
+
+
+public class MyQueue {
+    
+    private LinkedList<Integer> linkedList = new LinkedList<>();
+
+    // enqueue() - помещает элемент в конец очереди
+    void enqueue(int item) {
+        linkedList.addLast(item);
+    }
+
+    // dequeue() - возвращает первый элемент из очереди и удаляет его.
+    Integer dequeue() {
+        if (linkedList.size()>0) {
+            return linkedList.pollFirst();
+        }
+        return null;
+    }
+
+    //first() - возвращает первый элемент из очереди, не удаляя.
+    Integer first() {
+        if (linkedList.size()>0) {
+            return linkedList.peekFirst();
+        }
+        return null;
+    }
+
+    String doString(){
+        return linkedList.toString();
+    }
+
+    
+}
